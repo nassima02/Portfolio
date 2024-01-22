79717d028e23dda765e854a559eb9ca7b30e19ab
nassima bression <benasima@yahoo.fr>
2024-01-22 10:47:07 +0100

    feat[router] : configuration du router

---
+++ src/router.config/router.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../App'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home/>,
+	},
+])

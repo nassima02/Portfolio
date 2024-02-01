dc06e86f54822b568ab0f77d2fb69ab90cae24e3
nassima bression <benasima@yahoo.fr>
2024-02-01 10:12:30 +0100

    feat[contact] : creation de la section contact

--- src/components/animatedComponent.jsx
+++ src/components/animatedComponent.jsx
@@ -2,8 +2,8 @@ import "../App.css"
 const AnimatedComponent = () => {
 	return (
 		<div
-			className="absolute left-[50%] border-2 border-white rounded-lg h-10 w-6 ml-[-15px] block z-10 opacity-0.4 bottom-16">
-			<div className="relative rounded bg-white w-2 h-2 top-1 ml-auto mr-auto animateDrop"></div>
+			className="absolute left-[50%] border-2 border-white  rounded-lg h-8 w-4 md:h-10 md:w-6 ml-[-15px] block z-10 opacity-0.4 bottom-12 md:bottom-16">
+			<div className="relative rounded bg-white w-1.5 md:w-2 h-1.5 md:h-2  top-1 ml-auto mr-auto animateDrop"></div>
 		</div>
 	)
 };

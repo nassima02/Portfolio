872cdaea7f9ac8415f4ff5ac6f76180366f6933d
nassima bression <benasima@yahoo.fr>
2024-02-05 14:03:33 +0100

    feat[formulaire de contact]: refection du formulaire de contact avec React Hook Form

--- frontend/src/components/formulaire-de-contact.jsx
+++ frontend/src/components/formulaire-de-contact.jsx
@@ -1,40 +1,31 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const ContactForm = () => {
-	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
-		message: '',
-	});
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData({
-			...formData,
-			[name]: value,
-		});
-	};
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-
+	const onSubmit = async (data) => {
 		try {
-			const response = await fetch('/submit-form', {
+			const response = await fetch('https://bression.alwaysdata.net/submit-form', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify(formData),
+				body: JSON.stringify(data),
 			});
 
 			if (response.ok) {
 				console.log('Formulaire soumis avec succès !');
 				// Réinitialise les champs du formulaire après la soumission
-				setFormData({
-					name: '',
-					email: '',
-					message: '',
-				});
+				reset();
+				// Afficher un toast de succès
+				toast.success('Formulaire soumis avec succès !');
 			} else {
 				console.error('Erreur lors de la soumission du formulaire');
 			}
@@ -42,58 +33,55 @@ const ContactForm = () => {
 			console.error('Erreur lors de la soumission du formulaire', error);
 		}
 	};
+
 	return (
-<div>
-	<form className="flex flex-col md:w-[500px] w-full px-6 text-white text-lg" onSubmit={handleSubmit}>
-		<label className="text-left text-white">
-			Nom:<br/>
-			<input
-				className="w-[100%] h-8 text-[#2b3144]"
-				type="text"
-				name="name"
-				value={formData.name}
-				onChange={handleChange}
-			/>
-		</label>
-		<br/>
+		<div>
+			<form className="flex flex-col md:w-[500px] w-full px-6 text-white text-lg" onSubmit={handleSubmit(onSubmit)}>
+				<label className={`text-left text-white ${errors.name && 'border-red-500'}`}>
+					Nom:<br />
+					<input
+						className={`w-[100%] h-8 text-[#2b3144] ${errors.name && 'border-red-500'}`}
+						type="text"
+						name="name"
+						{...register('name', { required: 'Ce champ est obligatoire' })}
+					/>
+					{errors.name && <span className="text-[12px] text-red-500">{errors.name.message}</span>}
+				</label>
+				<br />
 
-		<label className="text-left text-white">
-			Email:<br/>
-			<input
-				className="w-[100%] h-8 text-[#2b3144]"
-				type="email"
-				name="email"
-				value={formData.email}
-				onChange={handleChange}
-			/>
-		</label>
-		<br/>
+				<label className={`text-left text-white ${errors.email && 'border-red-500'}`}>
+					Email:<br />
+					<input
+						className={`w-[100%] h-8 text-[#2b3144] ${errors.email && 'border-red-500'}`}
+						type="email"
+						name="email"
+						{...register('email', { required: 'Ce champ est obligatoire' })}
+					/>
+					{errors.email && <span className="text-[12px] text-red-500">{errors.email.message}</span>}
+				</label>
+				<br />
 
-		<label className="text-left text-white">
-			Message:<br/>
-			<textarea
-				className="w-[100%] h-32 text-[#2b3144]"
-				name="message"
-				value={formData.message}
-				onChange={handleChange}
-			/>
-		</label>
-		<br/>
+				<label className={`text-left text-white ${errors.message && 'border-red-500'}`}>
+					Message:<br />
+					<textarea
+						className={`w-[100%] h-32 text-[#2b3144] ${errors.message && 'border-red-500'}`}
+						name="message"
+						{...register('message', { required: 'Ce champ est obligatoire' })}
+					/>
+					{errors.message && <span className="text-[12px] text-red-500">{errors.message.message}</span>}
+				</label>
+				<br />
 
-		<button
-			className="text-center text-[#2b3144] bg-[#efeff1] w-28 self-center font-bold"
-			type="submit"
-		>
-			Envoyer
-		</button>
-	</form>
-
-	<div className="flex items-center gap-3 border-t border-white  m-6"></div>
-	<p className="text-white text-sm">
-		© 2024 Nassima BRESSION, Tous droits réservés.
-	</p>
-</div>
+				<button className="text-center text-[#2b3144] bg-[#efeff1] w-28 self-center font-bold" type="submit">
+					Envoyer
+				</button>
+			</form>
+			<ToastContainer />
+			<div className="flex items-center gap-3 border-t border-white m-6"></div>
+			<p className="text-white text-sm">© 2024 Nassima BRESSION, Tous droits réservés.</p>
+		</div>
 	);
 };
 
 export default ContactForm;
+
